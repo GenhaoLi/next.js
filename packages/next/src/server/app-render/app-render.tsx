f7f9c55251b01b329c763d7eb0677fee43377c3a
Jiachi Liu <inbox@huozhi.im>
2025-04-24 15:55:32 +0200

    remove the duplicated rendered metadata in error page

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -982,7 +982,7 @@ async function getErrorRSCPayload(
   const seedData: CacheNodeSeedData = [
     initialTree[0],
     <html id="__next_error__">
-      <head>{metadata}</head>
+      <head></head>
       <body>
         {process.env.NODE_ENV !== 'production' && err ? (
           <template
