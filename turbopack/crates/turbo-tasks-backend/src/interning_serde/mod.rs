743a346b0f7de334777a4fad78db91d3063b1bc6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-29 10:30:20 -0700

    global interning

--- turbopack/crates/turbo-tasks-backend/src/interning_serde/mod.rs
+++ turbopack/crates/turbo-tasks-backend/src/interning_serde/mod.rs
@@ -1,17 +1,17 @@
 //! Exposed for usage in `turbo-tasks-backend`
 
-use std::io::Write;
+use std::{
+    io::{Read, Write},
+    sync::LazyLock,
+};
 
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use smallvec::SmallVec;
+use dashmap::DashMap;
+use serde::{de::DeserializeOwned, Serialize};
 use turbo_rcstr::RcStr;
 use turbo_tasks::FxIndexSet;
 
 #[derive(Serialize)]
 struct SerData<'l>(&'l [u8], FxIndexSet<RcStr>);
-
-#[derive(Deserialize)]
-struct DeserData(SmallVec<[u8; 16]>, Vec<RcStr>);
 
 pub fn to_vec<T>(config: &pot::Config, value: &T) -> pot::Result<Vec<u8>>
 where
@@ -22,22 +22,67 @@ where
     Ok(vec)
 }
 
-pub fn to_writer<T, W>(config: &pot::Config, value: &T, writer: W) -> pot::Result<()>
+#[inline(never)] // Mutex outside of the hot path
+fn intern_str(s: &RcStr) -> u32 {
+    *GLOBAL_INTERN_MAP.entry(s.clone()).or_insert_with(|| {
+        let id = GLOBAL_INTERN_MAP.len() as u32;
+        GLOBAL_INTERN_MAP_REVERSE.insert(id, s.clone());
+        id
+    })
+}
+
+#[inline(never)] // Mutex outside of the hot path
+fn restore_str(id: u32) -> RcStr {
+    GLOBAL_INTERN_MAP_REVERSE.get(&id).unwrap().clone()
+}
+
+pub fn to_writer<T, W>(config: &pot::Config, value: &T, mut writer: W) -> pot::Result<()>
 where
     T: Serialize,
     W: Write,
 {
     let (result, ser_map) = turbo_rcstr::set_ser_map(|| config.serialize(value));
     let value = result?;
-    let data = SerData(&value, ser_map);
-    config.serialize_into(&data, writer)
+
+    let mut intern_map = Vec::with_capacity(ser_map.len());
+
+    for s in ser_map {
+        intern_map.push(intern_str(&s));
+    }
+
+    writer.write_all(&intern_map.len().to_le_bytes())?;
+    for &id in &intern_map {
+        writer.write_all(&id.to_le_bytes())?;
+    }
+
+    writer.write_all(&value)?;
+
+    Ok(())
 }
 
 pub fn from_slice<T>(config: &pot::Config, slice: &[u8]) -> pot::Result<T>
 where
     T: DeserializeOwned,
 {
-    let data: DeserData = config.deserialize(slice)?;
+    let mut reader = std::io::Cursor::new(slice);
 
-    turbo_rcstr::set_de_map(&data.1, || config.deserialize(&data.0))
+    let mut intern_map = Vec::new();
+
+    let mut len = [0; 4];
+    reader.read_exact(&mut len)?;
+    let len = u32::from_le_bytes(len);
+
+    for _ in 0..len {
+        let mut id = [0; 4];
+        reader.read_exact(&mut id)?;
+        intern_map.push(u32::from_le_bytes(id));
+    }
+
+    let mut de_map = Vec::with_capacity(intern_map.len());
+
+    for &id in &intern_map {
+        de_map.push(restore_str(id));
+    }
+
+    turbo_rcstr::set_de_map(&de_map, || config.deserialize_from(&mut reader))
 }
