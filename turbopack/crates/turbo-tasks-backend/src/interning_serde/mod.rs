163518ef666825b32b3ae852417645b58ab1c182
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-01 16:09:59 -0700

    Remove globals from turbo-persistence

--- turbopack/crates/turbo-tasks-backend/src/interning_serde/mod.rs
+++ turbopack/crates/turbo-tasks-backend/src/interning_serde/mod.rs
@@ -1,19 +1,12 @@
 //! Exposed for usage in `turbo-tasks-backend`
 
-use std::{
-    io::{Read, Write},
-    sync::LazyLock,
-};
+use std::io::{Read, Write};
 
-use dashmap::DashMap;
 use indexmap::IndexSet;
 use rustc_hash::FxBuildHasher;
 use serde::{de::DeserializeOwned, Serialize};
 use turbo_rcstr::RcStr;
 use turbo_tasks::FxIndexSet;
-
-#[derive(Serialize)]
-struct SerData<'l>(&'l [u8], FxIndexSet<RcStr>);
 
 pub fn to_vec<T>(config: &pot::Config, value: &T) -> anyhow::Result<(Vec<u8>, RcStrToLocalId)>
 where
@@ -22,14 +15,6 @@ where
     let mut vec = Vec::new();
     let ser_map = to_writer(config, value, &mut vec)?;
     Ok((vec, ser_map))
-}
-
-#[inline(never)] // Mutex outside of the hot path
-fn store_in_memory_cache(s: &RcStr, global_id: u32) -> u32 {
-    GLOBAL_INTERN_MAP_REVERSE.insert(global_id, s.clone());
-    *GLOBAL_INTERN_MAP
-        .entry(s.clone())
-        .or_insert_with(|| global_id)
 }
 
 #[derive(Default)]
@@ -83,28 +68,6 @@ where
     Ok(RcStrToLocalId(ser_map))
 }
 
-#[inline(never)] // Mutex outside of the hot path
-fn restore_strings_with_in_memory_cache(
-    global_ids: Vec<u32>,
-    mut query_db: impl FnMut(u32) -> anyhow::Result<RcStr>,
-) -> anyhow::Result<Vec<RcStr>> {
-    let missing = global_ids
-        .iter()
-        .copied()
-        .filter(|global_id| GLOBAL_INTERN_MAP_REVERSE.get(global_id).is_none());
-
-    for global_id in missing {
-        let s = query_db(global_id)?;
-        store_in_memory_cache(&s, global_id);
-    }
-
-    let mut result = Vec::with_capacity(global_ids.len());
-    for id in global_ids {
-        result.push(GLOBAL_INTERN_MAP_REVERSE.get(&id).unwrap().clone());
-    }
-    Ok(result)
-}
-
 pub fn from_slice<T>(
     config: &pot::Config,
     slice: &[u8],
@@ -117,7 +80,11 @@ where
 
     let global_ids = LocalIdToGlobalId::read_from(&mut reader)?;
 
-    let de_map = restore_strings_with_in_memory_cache(global_ids.0, query_db)?;
+    let de_map = global_ids
+        .0
+        .into_iter()
+        .map(query_db)
+        .collect::<anyhow::Result<Vec<_>>>()?;
 
     turbo_rcstr::set_de_map(&de_map, || Ok(config.deserialize_from(&mut reader)?))
 }
