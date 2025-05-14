8fa7bd5d5b30e21a1f60a64c4f655153bc5fe340
Will Binns-Smith <wbinnssmith@gmail.com>
2025-05-14 15:25:35 -0700

    next.config.ts: Implement `compiler.defineServer` for server-only constants
    Like `compiler.define`, this allows users to create compile-time constants both Turbopack and webpack from a single API. `compiler.defineServer` instead only makes these constants available for server or edge environments, not the client environment.

--- test/e2e/define/define.test.ts
+++ test/e2e/define/define.test.ts
@@ -5,17 +5,59 @@ describe('compiler.define', () => {
     files: __dirname,
   })
 
-  it('should render the magic variable on server side', async () => {
-    const res = await next.fetch('/')
-    const html = (await res.text()).replaceAll(/<!-- -->/g, '')
-    expect(html).toContain('Server value: foobar')
-    expect(html).toContain('Client value: foobar')
+  describe('compiler.define', () => {
+    it('should render the magic variable on server side', async () => {
+      const res = await next.fetch('/')
+      const html = (await res.text()).replaceAll(/<!-- -->/g, '')
+      expect(html).toContain('Server value: foobar')
+      expect(html).toContain('Client value: foobar')
+    })
+
+    it('should render the magic variable on client side', async () => {
+      const browser = await next.browser('/')
+      const text = await browser.elementByCss('body').text()
+      expect(text).toContain('Server value: foobar')
+      expect(text).toContain('Client value: foobar')
+    })
+
+    it('should render the magic expression on server side', async () => {
+      const res = await next.fetch('/')
+      const html = (await res.text()).replaceAll(/<!-- -->/g, '')
+      expect(html).toContain('Server expr: barbaz')
+      expect(html).toContain('Client expr: barbaz')
+    })
+
+    it('should render the magic expression on client side', async () => {
+      const browser = await next.browser('/')
+      const text = await browser.elementByCss('body').text()
+      expect(text).toContain('Server expr: barbaz')
+      expect(text).toContain('Client expr: barbaz')
+    })
   })
 
-  it('should render the magic variable on client side', async () => {
-    const browser = await next.browser('/')
-    const text = await browser.elementByCss('body').text()
-    expect(text).toContain('Server value: foobar')
-    expect(text).toContain('Client value: foobar')
+  describe('compiler.defineServer', () => {
+    it('should render the inlined variable on server side', async () => {
+      const res = await next.fetch('/with-server-only')
+      const html = (await res.text()).replaceAll(/<!-- -->/g, '')
+      expect(html).toContain('Server value: server')
+    })
+
+    it('should not render the inlined variable on client side', async () => {
+      const browser = await next.browser('/with-server-only')
+      const text = await browser.elementByCss('body').text()
+      expect(text).toContain('Client value: not set')
+    })
+
+    it('should render the inlined expression on server side', async () => {
+      const res = await next.fetch('/with-server-only')
+      const html = (await res.text()).replaceAll(/<!-- -->/g, '')
+      expect(html).toContain('Server expr: serverbarbaz')
+    })
+
+    it('should not render the inlined expression on client side', async () => {
+      const browser = await next.browser('/with-server-only')
+      const text = await browser.elementByCss('body').text()
+      expect(text).toContain('Client expr: not set')
+    })
   })
 })
